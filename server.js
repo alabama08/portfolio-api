f0b63e252eb972d54b10cb100708acc7386bc8ff
owoseni <owosenimotunrayo2@gmail.com>
2026-03-13 15:34:51 -0700

    url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin:      process.env.FRONTEND_URL || "http://localhost:5173",
+  origin:      process.env.FRONTEND_URL || "https://portfolio-u3qf.vercel.app",
   methods:     ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 }));
